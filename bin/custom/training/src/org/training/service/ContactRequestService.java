aa898a7c7c0745352b713811401f8dc905f6c016
Ilya Labovich <labovichl333@mail.ru>
2023-02-27 20:29:13 +0300

    Add properties use

--- bin/custom/training/src/org/training/service/ContactRequestService.java
+++ bin/custom/training/src/org/training/service/ContactRequestService.java
@@ -4,4 +4,6 @@ import org.training.model.ContactRequestModel;
 
 public interface ContactRequestService {
     ContactRequestModel getContactRequest(String sender);
+
+    ContactRequestModel getSampleContactRequest();
 }
--- bin/custom/training/src/org/training/service/impl/DefaultContactRequestService.java
+++ bin/custom/training/src/org/training/service/impl/DefaultContactRequestService.java
@@ -1,5 +1,6 @@
 package org.training.service.impl;
 
+import de.hybris.platform.servicelayer.config.ConfigurationService;
 import de.hybris.platform.servicelayer.exceptions.AmbiguousIdentifierException;
 import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
 import org.training.dao.ContactRequestDao;
@@ -10,20 +11,23 @@ import javax.annotation.Resource;
 import java.util.List;
 
 public class DefaultContactRequestService implements ContactRequestService {
+
+    private static final String SAMPLE_SENDER = "contactRequest.sample.sender";
+    private static final String SAMPLE_MESSAGE = "contactRequest.sample.message";
     @Resource
     private ContactRequestDao contactRequestDao;
+
+    @Resource
+    private ConfigurationService configurationService;
 
     public ContactRequestModel getContactRequest(final String sender) {
         final List<ContactRequestModel> result = contactRequestDao.findBySender(sender);
         final int resultCount = result.size();
-        if (resultCount == 0)
-        {
+        if (resultCount == 0) {
             throw new UnknownIdentifierException(
                     "ContactRequest with sender '" + sender + "' not found!"
             );
-        }
-        else if (resultCount > 1)
-        {
+        } else if (resultCount > 1) {
             throw new AmbiguousIdentifierException(
                     "ContactRequest with sender '" + sender + "' is not unique, " + resultCount
                             + " requests found!"
@@ -32,4 +36,11 @@ public class DefaultContactRequestService implements ContactRequestService {
         return result.get(0);
     }
 
+    @Override
+    public ContactRequestModel getSampleContactRequest() {
+        ContactRequestModel contactRequestModel = new ContactRequestModel();
+        contactRequestModel.setSender(configurationService.getConfiguration().getString(SAMPLE_SENDER));
+        contactRequestModel.setMessage(configurationService.getConfiguration().getString(SAMPLE_MESSAGE));
+        return contactRequestModel;
+    }
 }
