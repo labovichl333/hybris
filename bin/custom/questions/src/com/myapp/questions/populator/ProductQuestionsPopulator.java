6011f31f9c0a03a89c245bc3ecb94587f5a489fb
Ilya Labovich <labovichl333@mail.ru>
2023-03-09 14:52:23 +0300

    to find mistake

--- bin/custom/questions/src/com/myapp/questions/populator/ProductQuestionsPopulator.java
+++ bin/custom/questions/src/com/myapp/questions/populator/ProductQuestionsPopulator.java
@@ -2,6 +2,7 @@ package com.myapp.questions.populator;
 
 import com.myapp.questions.data.QuestionData;
 import com.myapp.questions.model.QuestionModel;
+import de.hybris.platform.commercefacades.product.converters.populator.AbstractProductPopulator;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.converters.Converters;
 import de.hybris.platform.converters.Populator;
@@ -11,13 +12,14 @@ import de.hybris.platform.servicelayer.dto.converter.Converter;
 
 import javax.annotation.Resource;
 
-public class ProductQuestionsPopulator implements Populator<ProductModel, ProductData> {
+public class ProductQuestionsPopulator<SOURCE extends ProductModel, TARGET extends ProductData> extends AbstractProductPopulator<SOURCE, TARGET> {
     private Converter<QuestionModel, QuestionData> converter;
 
     @Override
-    public void populate(ProductModel productModel, ProductData productData) throws ConversionException {
-        productData.setQuestions(Converters.convertAll(productModel.getQuestions(), converter));
+    public void populate(SOURCE source, TARGET target) throws ConversionException {
+        target.setQuestions(Converters.convertAll(source.getQuestions(), converter));
     }
+
 
     @Resource
     public void setConverter(Converter<QuestionModel, QuestionData> converter) {
